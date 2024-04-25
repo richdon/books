247a7e98df651d1e20d29a64e51c9161c5e6cb33
Richard Donald <richard_donald92@outlook.com>
2024-04-24 22:43:07 -0400

    edit book

--- src/App.js
+++ src/App.js
@@ -21,9 +21,20 @@ function App() {
         setBooks(updatedBooks);
     }
 
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, title: newTitle}
+            }
+            return book;
+
+        });
+        setBooks(updatedBooks);
+    };
+
     return <div className="app">
         <BookCreate onCreate={createBook}/>
-        <BookList books={books} onDelete={deleteBookById}/>
+        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
     </div>
 }
 
--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,4 +1,24 @@
-function BookEdit(){
-    return <div>BookEdit</div>;
+import {useState} from "react";
+
+function BookEdit({book, onEdit, onSubmit}) {
+    const [title, setTitle] = useState(book.title)
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onEdit(book.id, title);
+        onSubmit()
+    }
+
+    return <form className="book-edit" onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} className="input"/>
+        <button className="button is-primary">
+            Save
+        </button>
+    </form>;
 }
+
 export default BookEdit;
